b28b1c7706dd8194e0a65435faa6d008a71ac26b
DMAKCODESERIES <daniyalabbasi0349@gmail.com>
2025-02-08 20:49:52 +0500

    profile api completed

--- models/profile.model.js
+++ models/profile.model.js
@@ -6,12 +6,7 @@ const profileSchema = new mongoose.Schema(
     {
         user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        commission: { type: mongoose.Schema.Types.ObjectId, ref: "Referral", required: true },
-        commission: {
-            level1: { type: Number, default: 0 },
-            level2: { type: Number, default: 0 },
-            level3: { type: Number, default: 0 },
-        },
+        commission: { type: mongoose.Schema.Types.ObjectId, ref: "Referral", required: true }
     }
 )
 
